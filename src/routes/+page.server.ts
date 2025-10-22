f9190c728ea733bb69e79a65ea3feb0fd9ef95f0
Ibrahim <144132079+IZA165@users.noreply.github.com>
2025-10-22 14:13:07 +0100

    Update +page.server.ts

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,7 +3,6 @@ import { env } from '$env/dynamic/private';
 
 export const load: LayoutServerLoad = () => {
 	return {
-		title: env.TITLE ?? 'BiniLossless',
-		slogan: env.SLOGAN ?? 'The easiest way to stream CD-quality lossless FLACs.'
+		title: env.TITLE ?? 'BiniLossless'
 	};
 };
